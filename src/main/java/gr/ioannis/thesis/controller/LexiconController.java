704b111e0c65b31438a65446b089208b616d502b
Ioannis Mousmoutis <Ioannis.Mousmoutis@eurodyn.com>
2020-05-06 22:59:38 +0300

    Added endpoint to return active languages

--- src/main/java/gr/ioannis/thesis/controller/LexiconController.java
+++ src/main/java/gr/ioannis/thesis/controller/LexiconController.java
@@ -1,12 +1,15 @@
 package gr.ioannis.thesis.controller;
 
+import com.eurodyn.qlack.fuse.lexicon.dto.LanguageDTO;
 import com.eurodyn.qlack.fuse.lexicon.service.KeyService;
+import com.eurodyn.qlack.fuse.lexicon.service.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,14 +18,22 @@ public class LexiconController {
 
   private KeyService keyService;
 
+  private LanguageService languageService;
+
   @Autowired
-  public LexiconController(KeyService keyService) {
+  public LexiconController(KeyService keyService, LanguageService languageService) {
     this.keyService = keyService;
+    this.languageService = languageService;
   }
 
   @GetMapping()
-  public Map<String, String> getTranslations(@RequestParam String lang) {
+  public Map<String, String> getLexicon(@RequestParam String lang) {
     return keyService.getTranslationsForGroupNameAndLocale("ui", lang);
+  }
+
+  @GetMapping(value = "languages/")
+  public List<LanguageDTO> getLanguages() {
+    return languageService.getLanguages(false);
   }
 
 
