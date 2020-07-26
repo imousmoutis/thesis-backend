9d7916b93d59d557e020a977cc526b41479a9ac4
Ioannis Mousmoutis <Ioannis.Mousmoutis@eurodyn.com>
2020-07-26 16:46:02 +0300

    Updates

--- src/main/java/gr/ioannis/thesis/configuration/ThesisWebApplication.java
+++ src/main/java/gr/ioannis/thesis/configuration/ThesisWebApplication.java
@@ -1,6 +1,5 @@
 package gr.ioannis.thesis.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
